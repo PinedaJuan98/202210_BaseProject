5ffea652323704aca114e9ff5271d6d3af939b0e
Juan C Pineda Q <jc.pineda@uniandes.edu.co>
2022-05-01 20:04:15 -0500

    Ajustes finales del componente

--- src/app/planta/planta-list/planta-list.component.ts
+++ src/app/planta/planta-list/planta-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Planta } from '../planta';
+import { PlantaService } from '../planta.service';
 
 @Component({
   selector: 'app-planta-list',
@@ -10,9 +11,16 @@ export class PlantaListComponent implements OnInit {
 
   plantas: Array<Planta> = [];
 
-  constructor() { }
+  constructor(private plantaService: PlantaService) { }
 
-  ngOnInit() {
+  getPlantas(): void {
+    this.plantaService.getPlantas().subscribe((plantas) => {
+      this.plantas = plantas;
+    });
+  }
+
+  ngOnInit(): void {
+    this.getPlantas();
   }
 
 }
