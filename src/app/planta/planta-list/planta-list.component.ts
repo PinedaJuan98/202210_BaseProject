1139b0962b8a790cea3c6f0a946c2a83b6ce6084
Juan C Pineda Q <jc.pineda@uniandes.edu.co>
2022-05-01 19:59:37 -0500

    Se agrega servicio para utilizar http para obtener base de datos

--- src/app/planta/planta-list/planta-list.component.ts
+++ src/app/planta/planta-list/planta-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Planta } from '../planta';
 
 @Component({
   selector: 'app-planta-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planta-list.component.css']
 })
 export class PlantaListComponent implements OnInit {
+
+  plantas: Array<Planta> = [];
 
   constructor() { }
 
---
+++ src/app/planta/planta.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Planta } from './planta';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlantaService {
+
+  private apiUrl: string = environment.baseURL;
+
+  constructor(private http: HttpClient) { }
+
+  getPlantas(): Observable<Planta[]> {
+    return this.http.get<Planta[]>(this.apiUrl);
+  }
+
+}
